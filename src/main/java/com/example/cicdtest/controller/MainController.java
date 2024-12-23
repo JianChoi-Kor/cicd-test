262de1b2675ac083ca0d2215ff015580601d5aa8
JianChoi-Kor <wldks79@gmail.com>
2024-12-23 22:34:36 +0900

    cicdtest

---
+++ src/main/java/com/example/cicdtest/controller/MainController.java
@@ -0,0 +1,17 @@
+package com.example.cicdtest.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/main")
+public class MainController {
+
+    @GetMapping("/test1")
+    public String test1() {
+        return "test1";
+    }
+}
